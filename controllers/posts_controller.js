9f81fec605ee43818d552ee9fd56f7a355771681
Eliav Menachi <eliav.menachi@gmail.com>
2024-11-14 09:11:34 +0200

    mongoose

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,11 +1,43 @@
-const getAllPosts = (req, res) => {
-  console.log("get all posts");
-  res.send("get all posts");
+const PostModel = require("../models/posts_model");
+
+const getAllPosts = async (req, res) => {
+  const filter = req.query.owner;
+  try {
+    if (filter) {
+      const posts = await PostModel.find({ owner: filter });
+      res.send(posts);
+    } else {
+      const posts = await PostModel.find();
+      res.send(posts);
+    }
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
 };
 
-const createPost = (req, res) => {
-  console.log("create a post");
-  res.send("create a post");
+const getPostById = async (req, res) => {
+  const postId = req.params.id;
+
+  try {
+    const post = await PostModel.findById(postId);
+    if (post) {
+      res.send(post);
+    } else {
+      res.status(404).send("Post not found");
+    }
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+};
+
+const createPost = async (req, res) => {
+  const postBody = req.body;
+  try {
+    const post = await PostModel.create(postBody);
+    res.status(201).send(post);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
 };
 
 const deletePost = (req, res) => {
@@ -17,4 +49,5 @@ module.exports = {
   getAllPosts,
   createPost,
   deletePost,
+  getPostById,
 };
--- routes/posts_route.js
+++ routes/posts_route.js
@@ -2,16 +2,12 @@ const express = require("express");
 const router = express.Router();
 const postsController = require("../controllers/posts_controller");
 
-router.get("/", (req, res) => {
-  postsController.getAllPosts(req, res);
-});
+router.get("/", postsController.getAllPosts);
 
-router.post("/", (req, res) => {
-  postsController.createPost(req, res);
-});
+router.get("/:id", postsController.getPostById);
 
-router.delete("/", (req, res) => {
-  postsController.deletePost(req, res);
-});
+router.post("/", postsController.createPost);
+
+router.delete("/:id", postsController.deletePost);
 
 module.exports = router;
